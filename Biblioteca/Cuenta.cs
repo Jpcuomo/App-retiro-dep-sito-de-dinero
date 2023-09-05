98735a2e48f6b7a88773b5a8a6e24b40820f4795
Jpcuomo <jpcuomo2000@gmail.com>
2023-09-05 15:50:51 -0300

    Agregar archivos de proyecto.

---
+++ Biblioteca/Cuenta.cs
@@ -0,0 +1,81 @@
+﻿namespace Biblioteca
+{
+    public class Cuenta
+    {
+        private string _titular;
+        private decimal _cantidad;
+
+        /// <summary>
+        /// Clase que maneja el egreso e ingreso de dinero de clientes
+        /// </summary>
+        /// <param name="titular"></param>
+        /// <param name="cantidad"></param>
+        public Cuenta(string titular, decimal cantidad)
+        {
+            _titular = titular;
+            _cantidad = cantidad;
+        }
+
+        public string Titular
+        {
+            get
+            {
+                return _titular;
+            }
+            set
+            {
+                _titular = value;
+            }
+        }
+
+        public decimal Cantidad
+        {
+            get
+            {
+                return _cantidad;
+            }
+            set
+            {
+                _cantidad = value;
+            }
+        }
+
+        /// <summary>
+        /// Muestra el estado de cuenta del cliente
+        /// </summary>
+        /// <param name="titular"></param>
+        /// <param name="cantidad"></param>
+        /// <returns>Muestra información con el saldo actual de la cuenta</returns>
+        public void Mostrar()
+        {
+            Console.WriteLine("\nSaldo actual:");
+            Console.WriteLine($"Titular: {Titular} - Cantidad: {Cantidad:C2}");
+            Console.WriteLine("-----------------------------------------------------------");
+        }
+
+        /// <summary>
+        /// Metodo para agregar dinero a la cuenta
+        /// </summary>
+        /// <param name="monto"></param>
+        public void Ingresar(decimal monto)
+        {
+            if (monto >= 0)
+            {
+                Cantidad += monto;
+            }
+        }
+
+        /// <summary>
+        /// Metodo para extraer dinero de la cuenta
+        /// </summary>
+        /// <param name="monto"></param>
+        public void Retirar(decimal monto)
+        {
+            if (monto <= 0)
+            {
+                Cantidad += monto;
+            }
+        }
+
+    }
+}
---
+++ Ejercicio_I01/Program.cs
@@ -0,0 +1,53 @@
+﻿using Biblioteca;
+
+namespace Ejercicio_I01
+{
+    internal class Program
+    {
+        static string respuesta = "";
+
+
+        static void Main(string[] args)
+        {
+            Cuenta cuenta1 = new Cuenta("Juan Pérez", 1500000);
+
+            while (respuesta != "n")
+            {
+                cuenta1.Mostrar(); //Muestro saldo
+                Console.WriteLine("Ingrese monto a operar (monto positivo para ingresar/ negativo para extraer)");
+                try
+                {
+                    decimal? monto = decimal.Parse(Console.ReadLine());
+                    if (monto.HasValue)
+                    {
+                        if (monto >= 0)
+                        {
+                            cuenta1.Ingresar(monto.Value);
+                            Console.WriteLine($"Ingresó {monto.Value:C2}");
+                        }
+                        else
+                        {
+                            cuenta1.Retirar(monto.Value);
+                            Console.WriteLine($"Retiró {(-1 * monto.Value):C2}");
+                        }
+                        cuenta1.Mostrar();
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Monto inválido");
+                }
+
+                Console.WriteLine("Desea Realizar otra operación: (S/N)");
+                respuesta = Console.ReadLine();
+
+                if (respuesta.ToLower() == "n")
+                {
+                    Console.WriteLine("Gracias por elegirnos. Hasta la próxima!");
+                    break;
+                }
+            }
+
+        }
+    }
+}
